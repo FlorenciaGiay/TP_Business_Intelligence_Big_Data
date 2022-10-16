89fbb9cca4a781335781ebcbec46a34a6bd5ac9e
Gonzalo <knollgonzalo@gmail.com>
2022-10-16 20:39:20 -0300

    Fin ETL

--- Scripts SQL/ForEach que use para Entradas.sql
+++ Scripts SQL/ForEach que use para Entradas.sql
@@ -2,7 +2,7 @@ DECLARE @TicketList TABLE(nro_ticket varchar(20))
 DECLARE @nro_ticket varchar(20)
     
 INSERT @TicketList(nro_ticket)
-SELECT nro_ticket
+SELECT numero_ticket
 FROM Venta
     
 WHILE(1 = 1)
@@ -19,10 +19,11 @@ BEGIN
 			
 			DECLARE @itemList TABLE(codigo_item INT)
 			DECLARE @codigo_item INT
-			declare @tipo_visita int 
-			DECLARE @numeros table(numero int)
+			declare @producto int 
+			declare @control int
+			DECLARE @productosUsados table(codigo int)
 			
-			insert into @numeros 
+			/*insert into @numeros 
 			SELECT 1 as numero
 			UNION
 			SELECT 2
@@ -39,12 +40,12 @@ BEGIN
 			UNION
 			SELECT 8
 			UNION
-			select 9
+			select 9*/
 
 			INSERT @itemList(codigo_item)
 			SELECT id_item_venta
-			FROM Item_venta
-			where nro_ticket = @nro_ticket
+			FROM tabla_backup
+			where numero_ticket = @nro_ticket
     
 			WHILE(1 = 1)
 			BEGIN
@@ -56,13 +57,36 @@ BEGIN
 				IF @codigo_item IS NULL
 					BREAK
             
-				--@tipo_visita = ABS(CAST(NEWID() as binary(6)) % 9) + 1
-				SET @tipo_visita = NULL
-				select top(1) @tipo_visita = numero
-				from @numeros
-				order by newid()
-				update Item_venta set código_tipo_visita = @tipo_visita where id_item_venta = @codigo_item
-				delete from @numeros where numero = @tipo_visita
+				
+				SET @producto = NULL
+				select top(1) @producto = id_producto
+				from tabla_backup
+				where id_item_venta = @codigo_item
+				
+				--set @control = 0
+				while (1=1)
+				begin
+					if @producto not in (select codigo from @productosUsados)
+						begin
+							insert into @productosUsados select @producto
+							break
+							--set @control = 1
+						end
+					else
+						begin
+							if @producto < 44
+								begin
+									update tabla_backup set id_producto = @producto + 5 where id_item_venta = @codigo_item
+									set @producto = @producto + 5
+								end
+							else
+								begin
+									update tabla_backup set id_producto = @producto - 5 where id_item_venta = @codigo_item
+									set @producto = @producto - 5
+								end
+						end
+						--update tabla_backup set id_producto = case when id_producto < 44 then id_producto + 5 else id_producto - 5 end where id_item_venta = @codigo_item
+				end
     
 				DELETE TOP(1) FROM @itemList
     
@@ -73,4 +97,3 @@ BEGIN
     DELETE TOP(1) FROM @TicketList
     
 END
-
